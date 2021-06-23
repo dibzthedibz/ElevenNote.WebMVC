6a05f2ee222799e0fcadba9c536bc85e83102525
Ryan <Dibzthedibz@gmail.com>
2021-06-23 10:28:08 -0700

    NoteDetail Created, method added to services class and controller.

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -54,5 +54,12 @@ namespace ElevenNote.WebMVC.Controllers
             var service = new NoteService(userId);
             return service;
         }
+        public ActionResult Details (int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
     }
 }
