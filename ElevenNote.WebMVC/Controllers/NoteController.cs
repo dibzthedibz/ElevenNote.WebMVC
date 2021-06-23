1ad690b8dd65b4d13fad9de094fc8d088d35e35c
Ryan <Dibzthedibz@gmail.com>
2021-06-23 10:06:28 -0700

    NoteCreate Controller Method created.

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,5 +1,7 @@
 ﻿using ElevenNote.Data;
 using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +18,10 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
             return View(model);
         }
 
@@ -28,12 +33,18 @@ namespace ElevenNote.WebMVC.Controllers
         //Post: /Note
         [HttpPost, ActionName("Create")]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Note note)
+        public ActionResult Create(NoteCreate note)
         {
             if (ModelState.IsValid)
             {
-
+                return View(note);
             }
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            service.CreateNote(note);
+
+            return RedirectToAction("Index");
         }
     }
 }
